33efe52ed5b2d355803a839559fac2e9168ff023
Gallon-423 <020423cjl@gmail.com>
2022-10-28 12:34:15 +0800

    feat(components): new component BaseDateCircle

---
+++ src/components/BaseDateCircle.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props=defineProps<{
+  day: string;
+  month: string;
+  height: number;
+}>();
+
+
+</script>
+
+<template>
+  <div :style="{ height: height+'em', width: height+'em' }">
+    <div class="circle-dashed"></div>
+    <div class="circle-bk"></div>
+    <div class="up-semicircle-bk"></div>
+    <h2 class="day" :style="{fontSize:height/4+'em'}">{{ day }}</h2>
+    <h1 class="month" :style="{fontSize:height/5+'em'}">{{ month }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.circle-dashed {
+  background-color: transparent;
+  width: 100%;
+  height: 100%;
+  margin: 0 0 0 0;
+  position: absolute;
+  border-radius: 50%;
+  border: 2px dashed darkgray;
+  z-index: 0;
+}
+.circle-bk {
+  background-color: #E99D42;
+  width: 90%;
+  height: 90%;
+  position: absolute;
+  left: 5%;
+  top: 5%;
+  border-radius: 50%;
+  z-index: 1;
+}
+.up-semicircle-bk{
+  background-color: #BD3124;
+  width: 90%;
+  height: 45%;
+  position: absolute;
+  border-radius: 10000px 10000px 0 0;
+  left: 5%;
+  top: 5%;
+  z-index: 2;
+}
+.month{
+  left: 28%;
+  bottom: 20%;
+  position: absolute;
+  color: azure;
+  z-index:3;
+}
+.day{
+  left: 25%;
+  top: 10%;
+  position: absolute;
+  color: azure;
+  z-index:3;
+}
+
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+import BaseDateCircle from '../components/BaseDateCircle.vue'
+</script>
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>The page is for preview</h1>
+    <BaseDateCircle day="０１" month="10月" :height="6"></BaseDateCircle>
   </div>
 </template>
 
