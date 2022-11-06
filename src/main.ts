8ad02d056fc28a75203cd9ad0c6df01513383487
Gallon-423 <020423cjl@gmail.com>
2022-11-06 20:19:59 +0800

    refactor(main.ts): 引入使用element图标库

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './styles/element/index.scss'
 import ElementPlus from 'element-plus'
-
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
 const app = createApp(App)
+
+// 引入element组件
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+    console.log(key)
+}
 
 app.use(createPinia())
 app.use(router)
