3fbb61eb31b5184a2fc09145af65a628dfb85b62
Gallon-423 <020423cjl@gmail.com>
2022-11-16 17:01:09 +0800

    refactor: 原有组件调整

---
+++ src/components/MapMarked.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div style="width: 80vw; height: 80vh; overflow: scroll; margin-left: 10vw">
+        <div class="canves" style="height: 100%; position: absolute">
+            <img style="height: 100vh" src="../../local/map.png" alt="" />
+
+            <div
+                v-for="item in activities"
+                class="icon-container"
+                :style="{ top: item['y'], left: item['x'] }"
+            >
+                <el-button link>
+                    <LocationFilled color="red" class="icon" />
+                    <h1 class="activity_title" style="">
+                        {{ item['title'] }}
+                    </h1>
+                </el-button>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+import type { Activity } from "@/server/models"
+</script>
+<style lang=""></style>
