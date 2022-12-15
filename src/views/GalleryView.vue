e64566e70ab2617a1ef08dcb2ad902d44c72e725
Gallon-423 <020423cjl@gmail.com>
2022-12-15 11:50:32 +0800

    feat: 时光长廊增加全景图超链接

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,36 +1,46 @@
 <script setup lang="ts">
-import BaseBlockHeader from '@/components/BaseBlockHeader.vue';
-import NavMenu from '@/components/NavMenu.vue';
-import MapMarked from '@/components/MapMarked.vue';
-import BaseTail from '@/components/BaseTail.vue';
-import type { Gallery } from '@/server/models';
-import { ref } from 'vue';
-import { getGalleries } from '@/server/api';
-import BaseCarousel from '@/components/BaseCarousel.vue';
+import BaseBlockHeader from '@/components/BaseBlockHeader.vue'
+import NavMenu from '@/components/NavMenu.vue'
+import MapMarked from '@/components/MapMarked.vue'
+import BaseTail from '@/components/BaseTail.vue'
+import type { Gallery } from '@/server/models'
+import { ref } from 'vue'
+import { getGalleries } from '@/server/api'
+import BaseCarousel from '@/components/BaseCarousel.vue'
 const galleries = ref<Gallery[] | null>()
-getGalleries().then(res => {
-  galleries.value=res
+getGalleries().then((res) => {
+  galleries.value = res
 })
 </script>
 <template>
   <div class="top">
     <NavMenu></NavMenu>
     <BaseCarousel></BaseCarousel>
-    <BaseBlockHeader title="时光长廊" title_english="Gallery" :hide-more="true"/>
-    <MapMarked style="height:50vh;margin: 3vw;" :galleries="galleries" />
-    <BaseTail/>
+    <BaseBlockHeader title="时光长廊" title_english="Gallery" :hide-more="true" />
+    <MapMarked style="height: 50vh; margin: 3vw" :galleries="galleries" />
+    <ElButton style="margin: 3vw" link>
+      <div style="display: flex; flex-direction: column; align-items: center">
+        <a
+          style="font-weight: bold; font-size: 1.8em; text-decoration: underline"
+          href="https://www.expoon.com/9737/panorama"
+        >
+          前往全景地图链接
+        </a>
+        <div style="font-size: 1.6em;margin-top:2vh ;">（加载需要一点时间哦）</div>
+      </div>
+    </ElButton>
+    <BaseTail />
   </div>
 </template>
 
 <style scoped>
-
 .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n + 1) {
-    background-color: #d3dce6;
-  }
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
 .top {
   display: flex;
   flex-direction: column;
