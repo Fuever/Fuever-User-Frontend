e8de5be16205f552770350c6bbe5278f6e74ff1b
Gallon-423 <020423cjl@gmail.com>
2022-12-16 10:05:33 +0800

    refactor: 将走马灯图片改为图床链接

--- src/components/BaseCarousel.vue
+++ src/components/BaseCarousel.vue
@@ -9,13 +9,19 @@
   <div class="top">
     <el-carousel>
       <el-carousel-item v-for="item in 4" :key="item">
-        <el-image style="height: 100%;" :src="`@/../public/photo/${item}.jpg`" fit="fill"/>
+        <el-image style="height: 100%;" :src="carouselUrls[item-1]" fit="fill"/>
       </el-carousel-item>
     </el-carousel>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const carouselUrls = ["https://fuever-1313037799.cos.ap-nanjing.myqcloud.com/1671155023136925901.jpg",
+  "https://fuever-1313037799.cos.ap-nanjing.myqcloud.com/1671155522340950442.jpg",
+  "https://fuever-1313037799.cos.ap-nanjing.myqcloud.com/1671155550052697954.jpg",
+  "https://fuever-1313037799.cos.ap-nanjing.myqcloud.com/1671155577977827772.jpg"
+]
+</script>
 
 <style scoped>
 .top {
